23e4028588b398077ed08e2121731e8c51e4fffa
dubtcs <75275524+dubtcs@users.noreply.github.com.>
2025-01-02 14:05:18 -0500

    PA5 Start:

---
+++ PA5/cgen.h
@@ -0,0 +1,95 @@
+#include <assert.h>
+#include <stdio.h>
+#include "emit.h"
+#include "cool-tree.h"
+#include "symtab.h"
+
+enum Basicness     {Basic, NotBasic};
+#define TRUE 1
+#define FALSE 0
+
+class CgenClassTable;
+typedef CgenClassTable *CgenClassTableP;
+
+class CgenNode;
+typedef CgenNode *CgenNodeP;
+
+class CgenClassTable : public SymbolTable<Symbol,CgenNode> {
+private:
+   List<CgenNode> *nds;
+   ostream& str;
+   int stringclasstag;
+   int intclasstag;
+   int boolclasstag;
+
+
+// The following methods emit code for
+// constants and global declarations.
+
+   void code_global_data();
+   void code_global_text();
+   void code_bools(int);
+   void code_select_gc();
+   void code_constants();
+
+// My code pdoructions
+   void code_nametable();
+   void code_protos();
+
+// The following creates an inheritance graph from
+// a list of classes.  The graph is implemented as
+// a tree of `CgenNode', and class names are placed
+// in the base class symbol table.
+
+   void install_basic_classes();
+   void install_class(CgenNodeP nd);
+   void install_classes(Classes cs);
+   void build_inheritance_tree();
+   void set_relations(CgenNodeP nd);
+public:
+   CgenClassTable(Classes, ostream& str);
+   void code();
+   CgenNodeP root();
+};
+
+
+class CgenNode : public class__class {
+private: 
+   CgenNodeP parentnd;                        // Parent of class
+   List<CgenNode> *children;                  // Children of class
+   Basicness basic_status;                    // `Basic' if class is basic
+                                              // `NotBasic' otherwise
+   // inherits from class__class
+   // public:
+   // Symbol name;
+   // Symbol parent;
+   // Features features;
+   // Symbol filename;
+
+public:
+   CgenNode(Class_ c,
+            Basicness bstatus,
+            CgenClassTableP class_table);
+
+   void add_child(CgenNodeP child);
+   List<CgenNode> *get_children() { return children; }
+   void set_parentnd(CgenNodeP p);
+   CgenNodeP get_parentnd() { return parentnd; }
+   int basic() { return (basic_status == Basic); }
+
+public:
+   // Seems the other classes have their own code_X methods for assembly streams
+   // So ig we doing that
+   void code_proto(std::ostream& s);
+};
+
+class BoolConst 
+{
+ private: 
+  int val;
+ public:
+  BoolConst(int);
+  void code_def(ostream&, int boolclasstag);
+  void code_ref(ostream&) const;
+};
+
